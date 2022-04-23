55ddbe8d421fe8524c554c7f7b33d4f2e3d9aec2
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-04-23 17:05:39 -0400

    lookin good

--- src/components/steps/Account.jsx
+++ src/components/steps/Account.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import {useContext} from 'react';
+import { StepperContext } from '../../context/StepperContext';
 
 export default function Account() {
+  const { userData, setUserData } = useContext(StepperContext);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({...userData, [name]: value});
+  }
+
   return (
-    <div>Account</div>
+    <div className='flex flex-col'>
+      <div className='w-full mx-2 flex-1'>
+        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+          {" "}
+          Username
+        </div>
+        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+          <input 
+            onChange={handleChange}
+            value={userData["username"] || ""}
+            name="username"
+            placeholder="Username"
+            className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+          />
+        </div>
+      </div>
+
+      <div className='w-full mx-2 flex-1'>
+        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+          {" "}
+          Password
+        </div>
+        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+          <input 
+            onChange={handleChange}
+            value={userData["password"] || ""}
+            name="password"
+            placeholder="Password"
+            type="password"
+            className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+          />
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/steps/Details.jsx
+++ src/components/steps/Details.jsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import {useContext} from 'react'
+import { StepperContext } from '../../context/StepperContext'
 
 export default function Details() {
+  const { userData, setUserData } = useContext(StepperContext);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({...userData, [name]: value});
+  }
+
   return (
-    <div>Details</div>
+    <div>
+          <div className='flex flex-col'>
+      <div className='w-full mx-2 flex-1'>
+        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+          {" "}
+          Address
+        </div>
+        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+          <input 
+            onChange={handleChange}
+            value={userData["address"] || ""}
+            name="address"
+            placeholder="Address"
+            className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+          />
+        </div>
+      </div>
+
+      <div className='w-full mx-2 flex-1'>
+        <div className='font-bold h-6 mt-3 text-gray-500 text-xs leading-8 uppercase'>
+          {" "}
+          City
+        </div>
+        <div className='bg-white my-2 p-1 flex border border-gray-200 rounded'>
+          <input 
+            onChange={handleChange}
+            value={userData["password"] || ""}
+            name="city"
+            placeholder="City"
+            className='p-1 px-2 appearance-none outline-none w-full text-gray-800'
+          />
+        </div>
+      </div>
+    </div>
+    </div>
   )
 }
